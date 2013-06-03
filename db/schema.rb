7578d1d4f27043ea9cbf1c14e165759452b3e4f9
Samuel McTaggart <sam.mctaggart@gmail.com>
2013-06-03 15:27:05 -0400

    add roles act and user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130603140009) do
+ActiveRecord::Schema.define(:version => 20130603190523) do
 
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20130603140009) do
     t.datetime "updated_at",                             :null => false
     t.string   "first_name",                             :null => false
     t.string   "last_name",                              :null => false
+    t.string   "role",                                   :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
