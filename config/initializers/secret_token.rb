8b812cf74b9b390a07dc5e6de767022dfc361af5
Samuel McTaggart <sam.mctaggart@gmail.com>
2013-06-18 15:59:10 -0400

    add secret key

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookdMe::Application.config.secret_token = 'c748c879d0d0b871c5d6085058a4e8264ad957310a3ac4c1707d60a16f52bcd4d69e1d5e92a10a2ae55ae2e0f355cc81641ea6d71428f7f5831cfe4058438153'
