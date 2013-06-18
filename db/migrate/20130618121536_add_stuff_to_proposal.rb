3f4c529c946cef6176060bd947b0eb62fff14af7
Samuel McTaggart <sam.mctaggart@gmail.com>
2013-06-18 16:03:01 -0400

    fix migration

--- db/migrate/20130618121536_add_stuff_to_proposal.rb
+++ db/migrate/20130618121536_add_stuff_to_proposal.rb
@@ -1,7 +1,6 @@
 class AddStuffToProposal < ActiveRecord::Migration
   def change
     add_column :proposals, :price, :integer, null: false
-    add_column :proposals, :date, :datetime,  null: false
     add_column :proposals, :proposal, :text,  null: false
   end
 end
