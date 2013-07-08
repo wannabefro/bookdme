df4dd8b13fbba6212e84c731a1cba75758ca8075
Samuel McTaggart <sam.mctaggart@gmail.com>
2013-07-08 08:43:24 -0400

    production change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 BookdMe::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.action_mailer.default_url_options = { :host => 'production-bookdme.heroku.com' }
 
   # Code is not reloaded between requests
   config.cache_classes = true
